50c465b53a4c73234e6ec013e809eaf8ab29153a
AriskaAliefiaPutri <140670433+AriskaAliefiaPutri@users.noreply.github.com>
2024-02-19 11:38:27 +0700

    1.16
    PAGING

--- restoran/kategori/select.php
+++ restoran/kategori/select.php
@@ -1,8 +1,39 @@
+<div style="margin:auto; width:900px;">
+
 <?php 
 
     require_once "../function.php";
 
-    $sql = "SELECT * FROM tblkategori";
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi, $sql);
+
+    $jumlahdata = mysqli_num_rows($result);
+
+    
+
+
+    $mulai = 3;
+    $banyak = 3;
+
+    $halaman = ceil($jumlahdata / $banyak);
+
+    for ($i=1; $i <= $halaman ; $i++) {
+        echo '<a href="?p='.$i.'">'.$i.'</a>';
+        echo '&nbsp &nbsp &nbsp';
+    }
+
+    echo '<br>' '<br>';
+
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        $mulai = ($p * $banyak) - $banyak
+        //    6 = (3 * 3) - 3
+    }else{
+        $mulai =0;
+    }
+
+
+    $sql = "SELECT idkategori FROM tblkategori LIMIT $muali,$banyak";
 
     $result = mysqli_query($koneksi, $sql);
 
@@ -21,7 +52,7 @@
     </tr>
     
     ';
-    $no=1;
+    $no=$mulai+1;
     if (jumlah > 0) {
         while ($row = mysqli_fetch_assoc($result)){
             echo '<tr>'
@@ -34,3 +65,8 @@
     echo '</table>';
 
 ?>
+
+
+
+    <h1> belajar </h1>
+</div>
