2a9f44c4b2cf4cfa63bf7389865a44fb60b458e7
AriskaAliefiaPutri <140670433+AriskaAliefiaPutri@users.noreply.github.com>
2024-02-20 16:56:51 +0700

    1.19
    Object Oriented Programming PHP

---
+++ restoran/db.php
@@ -0,0 +1,58 @@
+<?php 
+
+    class DB {
+
+        // properti
+        private $host = "127.0.0.1";
+        private $user = "root";
+        private $password = "";
+        private $database = "dbrestoran";
+
+        public function __construct()
+        {
+            echo "function construct";
+        }
+
+        // method
+        public function selectData()
+        {
+            echo 'select data';
+        }
+
+        public function grtDatabase()
+        {
+            return $this->database;
+        }
+
+        public function tampil()
+        {
+            $this->selectData();
+        }
+
+        public static function insert()
+        {
+            echo "static function";
+        }
+
+
+    }
+
+    DB::insertData();
+
+    $db = new DB;
+
+    // echo '<br>';
+
+    // $db->selectData();
+
+    // echo '<br>';
+
+    // echo $db->host;
+
+    // echo '<br>';
+
+    // echo $db->getdatabase().'<br>;
+
+    // $db->tampil();
+
+?>
