ecd445dd821a36a71f9244d96b69b0974c86cf67
AriskaAliefiaPutri <140670433+AriskaAliefiaPutri@users.noreply.github.com>
2024-02-20 20:48:19 +0700

    1.22
    Mengatur Menu Aplikasi Restoran Berbasis Web

--- restoran/admin/index.php
+++ restoran/admin/index.php
@@ -1,3 +1,10 @@
+<?php 
+
+    require_once"../dbcontroller.php";
+    $db = new DB;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,18 +30,31 @@
             <div class="col-md-3">
                 
                 <ul class="nav flex-cloumn">
-                    <li class="nav-item"><a class="nav-link" href="#">Kategori</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">User</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">Kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">Pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=order&m=select">Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">Order Detail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=user&m=select">User</a></li>
                 </ul>
             </div>
         </div>
 
         <div class="row mt-5">
-            
+            <?php 
+                if (isset($_GET['f']) && isset($_GET['m'])) {
+                    $f=$_GET['f'];
+                    $m=$_GET['m'];
+
+                    $file = '../'.$f.'/'/$m.'.php';
+
+                    require_once $file;
+                }
+            ?>
+        </div>
+
+        <div class="row mt-5">
+
             <div class="col">
                 <p class="text-center"> 2019 - copyright@smkrevit.com</p>
             </div>
---
+++ restoran/pelanggan/select.php
@@ -0,0 +1,1 @@
+<h1> select kpelanggan</h1>
